0e66e32783fb5d224dc8c079d90b625e43367de0
SimpleCMS-awss <brian012@tutamail.com>
2025-11-24 15:37:56 +0100

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  businessName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  isVerified: {
+    type: Boolean,
+    default: false
+  },
+  verificationToken: String,
+  businessType: {
+    type: String,
+    enum: ['restaurant', 'retail', 'service', 'other'],
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+// Hash password antes de guardar
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
